9912fd33ed7bc482157410dbf7169d5f9f4c0f59
mopheshi <ndachimya@gmail.com>
2023-04-10 10:38:13 +0100

    Final Day (12)... Completed UWP GUI Development with C#... Yay!

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +14,50 @@ namespace Universal
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void pick_Click(object sender, RoutedEventArgs e)
+        {
+            // Randomized array of integers between 1 and 60
+            Random random = new Random();
+            int[] sequence = new int[60];
+
+            for (int i = 1; i <= sequence.Length; i++) sequence[i - 1] = i;
+            for (int j = 0; j < sequence.Length; j++)
+            {
+                int k = random.Next() % sequence.Length;
+                int l = sequence[j];
+                sequence[j] = k;
+                sequence[k] = l;
+            }
+
+            // Note: a Label is called TextBlock in UWP apps
+            // Insert statements to assign six array element values to the <TextBlock> components
+            one.Text = sequence[1].ToString();
+            two.Text = sequence[2].ToString();
+            three.Text = sequence[3].ToString();
+            four.Text = sequence[4].ToString();
+            five.Text = sequence[5].ToString();
+            six.Text = sequence[6].ToString();
+
+            // Button active states
+            pick.IsEnabled = false;
+            reset.IsEnabled = true;
+        }
+
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            // Reset TextBoxes to default text: "..."
+            one.Text = "...";
+            two.Text = "...";
+            three.Text = "...";
+            four.Text = "...";
+            five.Text = "...";
+            six.Text = "...";
+
+            // Button active states
+            pick.IsEnabled = true;
+            reset.IsEnabled = false;
         }
     }
 }
